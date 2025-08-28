9377d34521456dc812d460d9a2dba24043fee06a
khurrumzaka-rgb <khurrumzaka@gmail.com>
2025-08-27 22:29:53 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // good for GitHub Pages subpaths
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') }
-  }
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
